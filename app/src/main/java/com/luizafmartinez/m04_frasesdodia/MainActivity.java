81baa559ff715c3a29eafa2656f3d7fdeef26fe7
alxmart <alxmartinez@gmail.com>
2024-01-28 14:12:56 -0300

    Adding methods.

--- app/src/main/java/com/luizafmartinez/m04_frasesdodia/MainActivity.java
+++ app/src/main/java/com/luizafmartinez/m04_frasesdodia/MainActivity.java
@@ -3,12 +3,33 @@ package com.luizafmartinez.m04_frasesdodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] frases = {
+            "Only easy day was yesterday.",
+            "Missão dada é missão cumprida.",
+            "Qual é Curió, vai cantar o que pra nós?"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void gerarFrase(View view) {
+
+    }
+
+    public void exibirTodas(View view) {
+
+        for ( String frase: frases) {
+            System.out.println(frase);
+        }
+
+    }
+
+
 }
